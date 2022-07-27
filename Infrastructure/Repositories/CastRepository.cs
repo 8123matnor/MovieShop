2dea353d6b9fc0c2260280cb86a4250721ab380a
812 <89209963+8123matnor@users.noreply.github.com>
2022-07-27 10:07:07 -0600

    Cast Details Page Works

--- Infrastructure/Repositories/CastRepository.cs
+++ Infrastructure/Repositories/CastRepository.cs
@@ -18,8 +18,10 @@ public class CastRepository : ICastRepository
     {
         // select * from movie where id = 1 join genre, cast, moviegerne, moviecast
         var castDetails = await _movieShopDbContext.Casts
-            .Include(m => m.MoviesOfCast).ThenInclude(m => m.Cast)
+            .Include(m => m.MoviesOfCast).ThenInclude(m => m.Movie)
             .FirstOrDefaultAsync(m => m.Id == id);
+#pragma warning disable CS8603 // Possible null reference return.
         return castDetails;
+#pragma warning restore CS8603 // Possible null reference return.
     }
 }
